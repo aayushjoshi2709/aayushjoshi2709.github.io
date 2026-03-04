62d3283f56d2f1189dedb92f9bff9144fe30e7a1
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-03-04 10:53:05 +0530

    updated crousal css

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ const projectData = fetch("./data/projects.json")
       technology_div = "";
       for (tech of data.tech) {
         technology_div += `
-            <div class="p-2 rounded border-1 m-2">
+            <div class="p-1 px-4 rounded border-1 m-2">
                 ${tech}
             </div>
         `;
       }
       projectCrousal.innerHTML += `
-            <div class="flex-col shadow-lg rounded-xl p-6 w-sm border-1 space-between">
-                <img class="w-full" src=${data.image || "./images/github.png"}>
+            <div class="flex-col shadow-lg rounded-xl p-4 w-sm border-1 space-between">
+                <img  class="aspect-[4/3] w-full object-contain" src=${data.image || "./images/github.png"}>
                 <div class="flex-row space-between flex-1">
                     <h3 class="text-2xl text-bold m-4">${data.name}</h3>
                     <p class="text-md line-clamp-3">${data.description}</p>
-                    <div class="my-2 flex flex-wrap justify-center">
+                    <div class="my-3 flex flex-wrap justify-center">
                         ${technology_div}
                     <div>
                     <div class="mt-2 justify-center w-100">
                         <a target="_blank" href="${data.github}">
-                            <button class="text-xl m-4 transform hover:scale-110 transition duration-300 border-b-2 border-transparent hover:border-white">
+                            <button class="text-xl mx-4 transform hover:scale-110 transition duration-300 border-b-2 border-transparent hover:border-white">
                                 Github Link
                             </button>
                         </a>
 
                         <a target="_blank" href="${data.live || data.github}">
-                            <button class="text-xl m-4 transform hover:scale-110 transition duration-300 border-b-2 border-transparent hover:border-white">
+                            <button class="text-xl mx-4 transform hover:scale-110 transition duration-300 border-b-2 border-transparent hover:border-white">
                                 Live Link
                             </button>
                         </a>
